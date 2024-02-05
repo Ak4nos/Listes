ce6aab640b9a07fd58858d8bc3ce62bf1bd130b6
Ak4nos <81250555+Ak4nos@users.noreply.github.com>
2024-02-05 10:52:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+fruits = ["pomme", "banane", "orange"]
+#print(fruits)
+fruits.append("kiwi")
+#print(fruits)
+fruits.remove("orange")
+#print(fruits)
+fruits[1] = "ananas"
+#print(fruits)
+print(len(fruits))
+fruits.sort()
+print(fruits)
